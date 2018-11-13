c2651adfcfaaee29786a6cd6bb160bcc15b91dda
Peter Kehl <peter.kehl@gmail.com>
2018-11-13 15:09:33 -0800

    Readability

--- src/117-mouse-and-cheese.clj
+++ src/117-mouse-and-cheese.clj
@@ -18,32 +18,31 @@
                                                           :post[(string? %) (= (count %) (+ (count string) (count (str replacement)) -1))]}
                             (str (subs string 0 index) replacement (subs string (inc index))))
           result (loop [mx matrix
-                   recent-visits (for [x (range 0 height)
-                                       y (range 0 width)
-                                       :when (= (get (matrix x) y) \M)]
-                                   [x y])]
-              (println :mx mx)
-              (let [at (fn [[x y :as coords]] {:pre (place? coords)} (println :at x y mx)(get (mx x) y))
-                    is? (fn [pl character] (= (at pl) character))
-                    visited? (fn [pl] (is? pl \M)) ;as we visit places, we mark them with M
-                    pass? (fn [pl] (is? pl \space))
-                    goal? (fn [pl] (is? pl \C))
-                    recent-neighbours (for [visited recent-visits
-                                            neighb (neighbours-all visited)]
-                                        neighb)
-                    _ (println :recent-neighbours recent-neighbours)
-                    passable-neighbours (filter pass? recent-neighbours)
-                    ]
-                (if (seq (filter goal? recent-neighbours))
-                  true
-                  (if (seq passable-neighbours)
-                    (let [mx-with-visits (reduce (fn [mx [x y :as pl]] {:pre [(place? pl)]}
-                                                   (let [row (mx x)
-                                                         row-updated (replace-char-at row y \M)]
-                                                     (assoc mx x row-updated)))
-                                             mx passable-neighbours)]
-                      (recur mx-with-visits passable-neighbours))
-                    #_else false))))
+                        recent-visits (for [x (range 0 height)
+                                            y (range 0 width)
+                                            :when (= (get (matrix x) y) \M)]
+                                        [x y])]
+                   (println :mx mx)
+                   (let [at (fn [[x y :as coords]] {:pre (place? coords)} (println :at x y mx)(get (mx x) y))
+                         is? (fn [pl character] (= (at pl) character))
+                         visited? (fn [pl] (is? pl \M)) ;as we visit places, we mark them with M
+                         pass? (fn [pl] (is? pl \space))
+                         goal? (fn [pl] (is? pl \C))
+                         recent-neighbours (for [visited recent-visits
+                                                 neighb (neighbours-all visited)]
+                                             neighb)
+                         _ (println :recent-neighbours recent-neighbours)
+                         passable-neighbours (filter pass? recent-neighbours)]
+                      (if (seq (filter goal? recent-neighbours))
+                        true
+                        (if (seq passable-neighbours)
+                          (let [mx-with-visits (reduce (fn [mx [x y :as pl]] {:pre [(place? pl)]}
+                                                          (let [row (mx x)
+                                                                row-updated (replace-char-at row y \M)]
+                                                            (assoc mx x row-updated)))
+                                mx passable-neighbours)]
+                            (recur mx-with-visits passable-neighbours))
+                        #_else false))))
               ]
       result
     )
--- src/119-win-tic-tac-toe.clj
+++ src/119-win-tic-tac-toe.clj
@@ -1,34 +1,46 @@
-(def immediate-win ;each placement (on its own) must lead to an immediate win
-  (fn [piece mx] ;piece is 'o or 'x; mx is a matrix of symbols
-    (let [place? (fn [[x y :as coords]]
+(def immediate-wins "Return a set of coordinates [x y] of any placement(s), which (each separately) lead to an immediate win. piece is 'o or 'x; mx is a matrix (vector of rows) of symbols 'o, 'x or 'e for empty."
+  (fn [piece matrix]
+    (let [_ (assert (or (= piece 'o)
+                        (= piece 'x)))
+          has-three? (fn [threesome] (= (count threesome) 3))
+          _ (assert (and (has-three? matrix)
+                         (every? has-three? matrix)))
+          place? (fn [[x y :as coords]]
                    (and (= (count coords) 2) (<= 0 x 2) (<= 0 y 2)))
-          at( fn [matrix pl] {:pre [(place? pl)]} (get-in matrix pl))
-          directions (apply concat #_->seq-of-seq-of-places
-                            '(([0 0] [1 1] [2 2]) ;diagonal from top left
-                              ([2 0] [1 1] [0 2]))
-                            (for [i (range 0 3)]
-                              `(([~i 0] [~i 1] [~i 2]) ;horizontal
-                                ([0 ~i] [1 ~i] [2 ~i])) ;vertical
-                              ))
-          direction? (fn [dir] (and
-                                (= (count dir) 3)
-                                (every? place? dir)))
-          _ (assert (every? direction? directions))
-          empty-places (filter (comp (partial = :e) (partial at mx)) (for [x (range 0 3), y (range 0 3)] [x y]))
-          won-dir? (fn [mx dir] {:pre [(direction? dir)]}
-                     (let [mx-at (partial at mx)]
-                       (if (apply = (map mx-at dir))
-                         (let [value (mx-at (first dir))]
-                           (#{:o :x} value))))) ;we don't need to check who won, since we made the move
-          won-mx? (fn [mx]
-                    (some (partial won-dir? mx) directions))
+          at (fn [matrix place] {:pre [(place? place)]} (get-in matrix place))
+          triples (apply concat ; ->seq of seq of place
+                       '(([0 0] [1 1] [2 2])  ;diagonal from the top left
+                         ([2 0] [1 1] [0 2])) ;diagonal from the bottom left
+                       (for [i (range 0 3)]
+                         `(([~i 0] [~i 1] [~i 2])    ;horizontal: i-th row
+                           ([0 ~i] [1 ~i] [2 ~i])))) ;vertical: i-th column
+          triple? (fn [triple] (and (= (count triple) 3)
+                                    (every? place? triple)))
+          _ (assert (every? triple? triples))
+          won-triple? (fn [matrix triple] {:pre [(triple? triple)]}
+                        (let [matrix-at (partial at matrix)]
+                          (if (apply = (map matrix-at triple))
+                            (let [value (matrix-at (first triple))] ;All three pieces are equal, hence take the first one.
+                            (#{:o :x} value))))) ;Assuming no one won earlier, we don't need to check who won, since we made the move
+          won-matrix? (fn [matrix]
+                        (some (partial won-triple? matrix) triples))
+          empty-places (filter (comp (partial = :e) (partial at matrix)) (for [x (range 0 3), y (range 0 3)] [x y]))
           result (into #{}
-                    (for [pl empty-places
-                          :let [mx-updated (assoc-in mx pl piece)]
-                          :when (won-mx? mx-updated)]
-                      pl))
-          ]
-        result
-      )
-    )
-  )
+                   (for [place empty-places
+                         :let [matrix-updated (assoc-in matrix place piece)]
+                         :when (won-matrix? matrix-updated)]
+                     place))]
+      result)))
+
+
+
+
+
+
+
+
+
+
+#_(immediate-wins :x [[:o :e :e]
+                    [:o :x :o]
+                    [:x :x :e]])
