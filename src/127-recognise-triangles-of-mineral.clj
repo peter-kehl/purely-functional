6409a632b4354bcc3b8c62146e1dc83190f1b185
Peter Kehl <peter.kehl@gmail.com>
2018-11-17 20:31:07 -0800

    Compiles

--- src/127-recognise-triangles-of-mineral.clj
+++ src/127-recognise-triangles-of-mineral.clj
@@ -4,9 +4,9 @@
 ;Pick a corner point. Take one of 8 directions -> one side of triangle.
 ;The other side from the same corner has two options: the next (neighbouring) direction (45 degrees), or the second next direction (90 degrees).
 (fn [num-per-row]
-  (let [mx (vec (for [numeric-line num-per-row
-                      digit (seq (Integer/toString numeric-line 2))]
-                   ({\0 false \1 true} digit)))
+  (let [mx (vec (for [numeric-line num-per-row]
+                   (vec (for [digit (seq (Integer/toString numeric-line 2))]
+                      ({\0 false \1 true} digit)))))
         ; [x y] x is  row, y is a column
         directions #_from-top-left-clockwise [[-1 -1] [-1 0] [-1 1] [0 1] [1 1] [1 0] [1 -1] [0 -1]]
         numbered-direction #_rotate-index-overflow (fn [num] (directions (rem num 8)))
@@ -31,18 +31,44 @@
                          (recur (inc index) (next s)))
                        nil)
                      ))
-        turn (fn [prev-dir num-of-turnes] {:pre [(direction? prev-dir) (number? num-of-turns)] :post [(direction? %)]} ;previous direction => new direction 45degrees to the right
+        turn (fn [prev-dir num-of-turns] {:pre [(direction? prev-dir) (number? num-of-turns)] :post [(direction? %)]} ;previous direction => new direction 45degrees to the right
                (let [prev-dir-index (index-of prev-dir directions)]
                  (numbered-direction (+ prev-dir-index num-of-turns))))
         place? (fn [[x y :as pl]] {:pre [(and (= (count pl) 2) (number? x) (number? y))]}
                  (and (< -1 x height) (< -1 y width)))
         max-side (max height width) ;it's max, not min, because of example of 2 rows: 111, 011.
-        radius-range (seq (vec (range 0 max-side)))
-        within-triangle "TODO"
+        move (fn [[from-x from-y :as from] [dir-x dir-y :as dir]] {:pre [(place? from) (direction? dir)]};no :post validation by place?, because we validate outside
+               [(+ from-x dir-x) (+ from-y dir-y)])
+        ;radius-range (seq (vec (range 0 max-side)))
+        immediate-neighbours (fn [from] {:pre [(place? from)]}
+                               (filter place?
+                                       (for [dir directions]
+                                         (move from dir))))
         triangle-sizes (for [corner-x x-range
                              corner-y y-range
+                             :let [corner [corner-x corner-y]]
+                             :when [(at corner)]
                              dir-left-side  directions
                              [dir-right-side dir-left-right] [[(turn dir-left-side 1) (turn dir-left-side 2)] ;45degrees
-                                                              [(turn dir-left-side 2) (turn dir-left-side 3)]]];90degrees
-                         (for [radius radius-range]))
-      ])))
+                                                              [(turn dir-left-side 2) (turn dir-left-side 3)]];90degrees
+                             size (loop [left-corner corner
+                                         right-corner corner
+                                         size-so-far 1]
+                                    (let [left-corner-new  (move  left-corner  dir-left-side)
+                                          right-corner-new (move right-corner dir-right-side)
+                                          left-to-right-length (loop [place left-corner-new ;loop returns nil if the line is not purely mineral
+                                                                      length 0]
+                                                                 (let [place-new (move place dir-left-right)]
+                                                                   (assert place? place-new)
+                                                                   (if (at place)
+                                                                     (if (= place right-corner-new)
+                                                                       (inc length)
+                                                                       (recur place-new (inc length)))
+                                                                     nil)))]
+                                      (if left-to-right-length
+                                        (recur left-corner-new right-corner-new (+ size-so-far left-to-right-length))
+                                        size-so-far)))
+                             :when (< 1 size)]
+                           size )
+        ]
+    (triangle-sizes mx))))
